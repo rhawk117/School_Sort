3183ce11555d78e0d5e72f5ea59f31802198e9e4
rhawk117 <hawkinsry19@gmail.com>
2024-01-22 13:33:00 -0500

    Developed method and logic of sorting the files with the presets, needs to be refactored because it's hard to follow

--- preset.py
+++ preset.py
@@ -7,13 +7,21 @@ import json
 import re as regex
 from pprint import pprint
 from InquirerPy import prompt
+from dataclasses import dataclass
 
 
 # handles all logic related to loading and creating a preset
 class Preset:
+    # Class Properties
+    # avlble_preset => list of all found .json files found in the presets dir
+    # slcted_preset => the preset the user has selected from avlble_preset
+    # loaded_preset => the json preset that was selected and successfully opened
+    # created_preset => the preset that was created by the user 
+    # target_files => list of TargetFile Objects that will be moved 
+
 
     # private function that walks through the preset directory
-    # sets the avlble_preset field to generate list of user choices\
+    # sets the avlble_preset field to generate list of user choices
     def _fetch_presets(self) -> None:
         # change to .py file dir
         os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
@@ -71,7 +79,7 @@ class Preset:
         try:
             tmp = {}
             with open(file='presets\\' + self.slcted_preset, mode='r') as preset:
-                print(f'[*] Succesfully Opened Preset [*]')
+                print(f'[i] Succesfully Opened Preset [i]'.center(60))
                 tmp = json.load(preset)
 
         except Exception:
@@ -86,7 +94,60 @@ class Preset:
         # set value to class attribute to allow us to chain multiple functions together
         self.loaded_preset = tmp
 
+    def comb_dir(self, dir_path):
+        if not self.loaded_preset:
+            print(
+                "[!] ERROR: Cannot use a null preset, try again with a valid preset [!]"
+            )
+            return
+
+        movable = []  # find a list of movable files
+        try:
+            for root, _, files in os.walk(dir_path):
+                for file in files:
+                    # All sorted files should contain an underscore and the first 4 digits should be the course number
+                    if '_' in file and file[0:3].isdigit():
+                        tmp = file.split('_')
+                        # when the length is less than 3 that means either there isn't a crn, abreviated output dir or file name
+                        # tmp[0] is the course number which should be a key in the dictionary
+                        if not tmp or len(tmp) < 3 or tmp[0] not in self.loaded_preset.keys():
+                            print(f'> Skipped {file}')
+                            continue
+
+                        crse_data = self.loaded_preset.get(tmp[0])
+                        # The 2nd index of tmp is the abreviated key for the output directory, check to see if it's in the file
+                        if tmp[1] not in crse_data.keys():
+                            print(f' > Skipped {file}, {tmp[1]} not in keys')
+                            continue
+
+                        # tmp[1] is the abreviated destination directory which we use to access it's value, the destination path
+                        destination = crse_data.get(tmp[1])
+                        source = os.path.abspath(
+                            os.path.join(root, file)
+                        )
+                        if not os.path.exists(destination):
+                            raise Exception("[!] CRITICAL ERROR: The destination file path in the json preset does not exist [!]\n" +
+                                            "[!] The preset loaded should not be used again and needs to be reconfigured [!]"
+                                            )
+
+                        # Create our Data Class for the file we want to move with all the information we need
+                        tmp = TargetFile(destination, source)
+                        movable.append(tmp)
+            if not movable:
+                print(
+                    f"[!] ERROR: No files were found or could be parsed [!]".center(60))
+            # All checks have been passed
+            self.target_files = movable
+        
+        except Exception as ex:
+            print(
+                f'[!] ERROR: Failed to fecth files from ({dir_path}), cannot sort files [!]')
+            print(ex)
+            return []
     
+
+    
+
     # public function that uses the private class methods used for handling the logic of loading a preset 
     def load_preset_hndler(self):
         # when load preset in the main menu is selected 
@@ -127,7 +188,7 @@ class Preset:
             raise Exception("[!] Cannot Proccess User Request [!]".center(60))
         
 
-        data = {}
+        self.loaded_preset = {}
         for path, subdirs, _ in os.walk(dir_path):
             # Get the depth of the current directory by counting os.sep in path
             depth = path[len(dir_path):].count(os.sep)
@@ -159,19 +220,19 @@ class Preset:
                     sub_value = os.path.join(path, sub) # save the path to the directory 
                     sub_dir_data[sub_key] = sub_value
 
-                # Add subdirectory data to main data dictionary
-                data[sub_dir_name] = sub_dir_data
+                # Add subdirectory self.loaded_preset to main self.loaded_preset dictionary
+                self.loaded_preset[sub_dir_name] = sub_dir_data
 
 
         print('\n[*] Preset Data Succesfully Generated [*]\n'.center(60))
-        pprint(data, indent = 6)
+        pprint(self.loaded_preset, indent = 6)
 
         # No Data was created
-        if not data:
+        if not self.loaded_preset:
             print(f"[!] ERROR: Failed to create any presets, try again [!]")
             sys.exit()
 
-        self.created_preset = data
+        self.created_preset = self.loaded_preset
 
     # private function that creates the file after recieving all preset information
     def _create_preset_file(self) -> None:
@@ -183,7 +244,7 @@ class Preset:
                 '[?] Enter the desired file name of the json preset with ".json" at the end: '
             )
 
-        # Create and dump dictionary data into the newly named and created json
+        # Create and dump dictionary self.loaded_preset into the newly named and created json
         try:
             with open(file=f'presets\\{file_name}', mode='w') as file_preset:
                 json.dump(self.created_preset, file_preset, indent = 4)
@@ -200,7 +261,17 @@ class Preset:
         self._auto_preset_data()
         self._create_preset_file()
 
+        
+            
 
+# representation of the source and destination of files that will be moved
+@dataclass
+class TargetFile:
+   destination: str
+   source: str
+   # ToString Method to display all of the files that are about to be moved to the user
+   def __str__(self) -> str:
+       return f"[ File => {os.path.basename(self.source)} is being moved to => {self.destination} ]".center(60)
         
 
     
